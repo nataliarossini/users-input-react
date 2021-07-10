fe51467643251383634e94df95b23fd0b8412cb8
Natalia Rossini <naty.rossini@hotmail.com>
2021-07-10 15:30:15 +0100

    fixed empty card

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div>
       <AddUser onAddUser={addUserHandler}/>
-      <UsersList users={usersList} />
+      {usersList.length > 0 && <UsersList users={usersList} />}
     </div>
   );
 }
